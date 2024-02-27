b59ec7281c9abfe33944ae6506baca4484590d36
Andre home <andrebackmann@gmail.com>
2024-02-27 19:34:44 +0200

    add constant chords for key of C

--- src/main/kotlin/com/example/Constants.kt
+++ src/main/kotlin/com/example/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     val VALID_KEYS = setOf("C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B")
 
     val VALID_SCALES = setOf("major", "minor")
+
+    val C_MAJOR_CHORDS = setOf("C", "Dm", "Em", "F", "G", "Am", "Bmb5")
 }
