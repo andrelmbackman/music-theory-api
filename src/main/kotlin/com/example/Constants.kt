24b05347b07a4e80fdb33898f137f619e6de3853
Andre home <andrebackmann@gmail.com>
2024-02-28 14:25:08 +0200

    add constants for major and minor scales containing sharps and flats, respectively

--- src/main/kotlin/com/example/Constants.kt
+++ src/main/kotlin/com/example/Constants.kt
@@ -9,5 +9,17 @@ object Constants {
 
     val FLAT_NOTES = setOf("C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B")
 
-    val C_MAJOR_CHORDS = setOf("C", "Dm", "Em", "F", "G", "Am", "Bmb5")
+    val MAJOR_KEY_CONTAINS_SHARP = setOf("G", "D", "A", "E", "B", "F#")
+
+    val MAJOR_KEY_CONTAINS_FLAT = setOf("F", "Bb", "Eb", "Ab", "Db", "Gb")
+
+    val MINOR_KEY_CONTAINS_SHARP = setOf("E", "B", "F#", "C#", "G#", "D#", "A#")
+
+    val MINOR_KEY_CONTAINS_FLAT = setOf("D", "G", "C", "F", "Bb", "Eb", "Ab")
+
+    val MAJOR_CHORD_EXTENSIONS = setOf("", "m", "m", "", "", "m", "mb5")
+
+    val MINOR_CHORD_EXTENSIONS = setOf("m", "mb5", "", "m", "m", "", "")
 }
+
+//TODO: C# = Db, D# = Eb, G# = Ab, A# = Bb, (Cb = B, Fb = E, E# = F, B# = C)
