dd295b4b8576cf0de8cdd1dcd1da1f2cb5c47ffb
Andre home <andrebackmann@gmail.com>
2024-02-28 12:50:50 +0200

    update validation

--- src/main/kotlin/com/example/ChordProgression.kt
+++ src/main/kotlin/com/example/ChordProgression.kt
@@ -2,16 +2,16 @@ package com.example
 import com.example.Constants.VALID_KEYS
 import com.example.Constants.VALID_SCALES
 
-fun generateChordProgression(key: String, scale: String, length: Int): List<String> {
+/*fun generateChordProgression(key: String, scale: String, length: Int): List<String> {
     // Your chord progression generation logic here
-}
+}*/
 
 fun validateKey(key: String): Boolean {
-    return key.uppercase() in VALID_KEYS
+    return key in VALID_KEYS
 }
 
 fun validateScale(scale: String): Boolean {
-    return scale.uppercase() in VALID_SCALES
+    return scale.lowercase() in VALID_SCALES
 }
 //TODO: Create data structure for properly transposing keys and giving their sharps and flats.
 // enum equivalent? I II III IV V VI VII
