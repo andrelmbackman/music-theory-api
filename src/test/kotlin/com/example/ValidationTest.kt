70aef296d2226aa9b764b26faa082d9f1a875858
Andre home <andrebackmann@gmail.com>
2024-02-28 12:51:10 +0200

    add tests to validate key and scale

--- src/test/kotlin/com/example/ValidationTest.kt
+++ src/test/kotlin/com/example/ValidationTest.kt
@@ -1,4 +1,31 @@
 package com.example
 
+import org.junit.Test
+import kotlin.test.*
 class ValidationTest {
+    @Test
+    fun testValidKeys() {
+        val validKeys = listOf("C", "C#", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B")
+        validKeys.forEach {
+            assertTrue(validateKey(it), "Failed for key: $it")
+        }
+    }
+
+    @Test
+    fun testInvalidKeys() {
+        val invalidKeys = listOf("Q", "W", "l", "1", "Ö", "Gee", "Major", "minor")
+        invalidKeys.forEach {
+            assertFalse(validateKey(it), "Failed for key: $it")
+        }
+    }
+
+    @Test
+    fun testScales() {
+        assertTrue(validateScale("major"))
+        assertTrue(validateScale("minor"))
+
+        assertFalse(validateScale("whole tone"))
+        assertFalse(validateScale("microtonal"))
+        assertFalse(validateScale("hypermegametamixolydian"))
+    }
 }
