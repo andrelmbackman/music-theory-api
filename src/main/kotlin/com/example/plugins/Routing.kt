0fde5454dfd007b71f4afe5b6251e9dae2c88340
Andre home <andrebackmann@gmail.com>
2024-02-29 18:02:41 +0200

    get request returns the notes of a given scale and key

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,11 +3,15 @@ package com.example.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import com.example.*
 
 fun Application.configureRouting() {
     routing {
+        val gen = NoteGenerator()
+        val text = gen.getNotes("A", "minor")
         get("/") {
-            call.respondText("Hello, World!")
+            //call.respondText("Hello, World!")
+            call.respondText(text.toString())
         }
     }
 }
