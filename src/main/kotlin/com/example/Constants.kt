74f441a5673c5f7d6281a90201bba38c2877e284
Andre home <andrebackmann@gmail.com>
2024-02-29 18:02:13 +0200

    add constants: indexes for the tones of a major and minor scale

--- src/main/kotlin/com/example/Constants.kt
+++ src/main/kotlin/com/example/Constants.kt
@@ -2,24 +2,28 @@ package com.example
 
 object Constants {
     val VALID_KEYS = setOf("C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B")
-
     val VALID_SCALES = setOf("major", "minor")
 
-    val SHARP_NOTES = setOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
-
-    val FLAT_NOTES = setOf("C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B")
+    val SHARP_NOTESET = setOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+    val FLAT_NOTESET = setOf("C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B")
+    val MAJOR_TONES_INDEXES = listOf(1, 3, 5, 6, 8, 10, 12)
+    val MINOR_TONES_INDEXES = listOf(1, 3, 4, 6, 8, 9, 11)
 
     val MAJOR_KEY_CONTAINS_SHARP = setOf("G", "D", "A", "E", "B", "F#")
-
     val MAJOR_KEY_CONTAINS_FLAT = setOf("F", "Bb", "Eb", "Ab", "Db", "Gb")
-
     val MINOR_KEY_CONTAINS_SHARP = setOf("E", "B", "F#", "C#", "G#", "D#", "A#")
-
     val MINOR_KEY_CONTAINS_FLAT = setOf("D", "G", "C", "F", "Bb", "Eb", "Ab")
-
     val MAJOR_CHORD_EXTENSIONS = setOf("", "m", "m", "", "", "m", "mb5")
-
     val MINOR_CHORD_EXTENSIONS = setOf("m", "mb5", "", "m", "m", "", "")
 }
 
-//TODO: C# = Db, D# = Eb, G# = Ab, A# = Bb, (Cb = B, Fb = E, E# = F, B# = C)
+//TODO: C# maj = Db, D# maj = Eb, G# maj = Ab, A# maj = Bb, (Cb = B, Fb = E, E# = F, B# = C), add note text if changed
+//
+/**
+ * CHORD TYPES:
+ * major
+ * minor
+ * major7, major6
+ * minor7, minor6
+ *
+ */
