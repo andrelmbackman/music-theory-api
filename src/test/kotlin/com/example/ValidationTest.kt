9f781c70285ab8ef7091f1e2fdd74ede35ee6721
Andre home <andrebackmann@gmail.com>
2024-02-29 12:54:30 +0200

    add test to validate correct key signatures given the key and scale

--- src/test/kotlin/com/example/ValidationTest.kt
+++ src/test/kotlin/com/example/ValidationTest.kt
@@ -1,7 +1,12 @@
 package com.example
 
+import com.example.Constants.FLAT_NOTESET
+import com.example.Constants.SHARP_NOTESET
 import org.junit.Test
 import kotlin.test.*
+import com.example.NoteGenerator
+
+
 class ValidationTest {
     @Test
     fun testValidKeys() {
@@ -17,6 +22,38 @@ class ValidationTest {
         invalidKeys.forEach {
             assertFalse(validateKey(it), "Failed for key: $it")
         }
+    }
+
+    @Test
+    fun testKeySignature() {
+        val gen = NoteGenerator()
+        assert(gen.getNoteSet("F", "major") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Bb", "major") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Eb", "major") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Ab", "major") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Db", "major") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Gb", "major") == FLAT_NOTESET)
+
+        assert(gen.getNoteSet("D", "minor") == FLAT_NOTESET)
+        assert(gen.getNoteSet("G", "minor") == FLAT_NOTESET)
+        assert(gen.getNoteSet("C", "minor") == FLAT_NOTESET)
+        assert(gen.getNoteSet("F", "minor") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Bb", "minor") == FLAT_NOTESET)
+        assert(gen.getNoteSet("Eb", "minor") == FLAT_NOTESET)
+
+        assert(gen.getNoteSet("G", "major") == SHARP_NOTESET)
+        assert(gen.getNoteSet("D", "major") == SHARP_NOTESET)
+        assert(gen.getNoteSet("A", "major") == SHARP_NOTESET)
+        assert(gen.getNoteSet("E", "major") == SHARP_NOTESET)
+        assert(gen.getNoteSet("B", "major") == SHARP_NOTESET)
+        assert(gen.getNoteSet("F#", "major") == SHARP_NOTESET)
+
+        assert(gen.getNoteSet("E", "minor") == SHARP_NOTESET)
+        assert(gen.getNoteSet("B", "minor") == SHARP_NOTESET)
+        assert(gen.getNoteSet("F#", "minor") == SHARP_NOTESET)
+        assert(gen.getNoteSet("C#", "minor") == SHARP_NOTESET)
+        assert(gen.getNoteSet("G#", "minor") == SHARP_NOTESET)
+        assert(gen.getNoteSet("D#", "minor") == SHARP_NOTESET)
     }
 
     @Test
