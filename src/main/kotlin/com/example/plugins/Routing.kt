18784cde84b7b5429af566fc2e4c0e30642e5f62
Andre home <andrebackmann@gmail.com>
2024-03-11 21:41:10 +0200

    add endpoints for /scales/[key]/[scale type]/notes

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,14 +4,20 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import com.example.*
+import io.ktor.http.*
 
 fun Application.configureRouting() {
     routing {
         val gen = NoteGenerator()
-        val text = gen.getNotes("A", "minor")
-        get("/") {
-            //call.respondText("Hello, World!")
-            call.respondText(text.toString())
+        get("/scales/{key}/{type}/notes") {
+            val key = call.parameters["key"]
+            val scale = call.parameters["type"]
+            if (scale != null && key != null && validateKey(key) && validateScale(scale))
+                call.respond(HttpStatusCode.OK, gen.getNotes(gen.replaceKeyName(key), scale))
+            else
+                call.respond(HttpStatusCode.NotFound, "No such key and scale combination found.")
         }
     }
 }
+
+//TODO: /scales/find "POST" [list of notes], return all scales and modes with these in them. If none applied, chromatic.
