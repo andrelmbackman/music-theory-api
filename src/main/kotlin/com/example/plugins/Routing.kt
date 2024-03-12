88d8046755284c49eaf38ece3bb7697619530bcb
Andre home <andrebackmann@gmail.com>
2024-03-12 15:26:04 +0200

    rm unused imports

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,9 +1,9 @@
 package com.example.plugins
 
+import com.example.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import com.example.*
 import io.ktor.http.*
 
 fun Application.configureRouting() {
